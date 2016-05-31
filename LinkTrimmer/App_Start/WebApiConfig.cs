741475b236f7195e8a5a7013e4e21a9ed70ceda8
iRodchenkov <tolstiignom@gmail.com>
2016-06-01 00:05:25 +0300

    angular app

--- LinkTrimmer/App_Start/WebApiConfig.cs
+++ LinkTrimmer/App_Start/WebApiConfig.cs
@@ -9,9 +9,6 @@ namespace LinkTrimmer
     {
         public static void Register(HttpConfiguration config)
         {
-            // Web API configuration and services
-
-            // Web API routes
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
